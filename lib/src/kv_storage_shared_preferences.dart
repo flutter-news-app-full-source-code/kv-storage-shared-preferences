f611a3b59c3a90fc0cf6050bde9852d7326ab982
fulleni <fulleni@hotmail.com>
2025-03-27 09:06:00 +0100

    feat: initial project setup
    - Added github workflows
    - Added issue templates
    - Added dependabot config

---
+++ lib/src/kv_storage_shared_preferences.dart
@@ -0,0 +1,7 @@
+/// {@template kv_storage_shared_preferences}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class KvStorageSharedPreferences {
+  /// {@macro kv_storage_shared_preferences}
+  const KvStorageSharedPreferences();
+}
---
+++ test/src/kv_storage_shared_preferences_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:kv_storage_shared_preferences/kv_storage_shared_preferences.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('KvStorageSharedPreferences', () {
+    test('can be instantiated', () {
+      expect(KvStorageSharedPreferences(), isNotNull);
+    });
+  });
+}

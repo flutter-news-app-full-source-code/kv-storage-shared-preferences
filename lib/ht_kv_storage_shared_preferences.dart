c84f86bc0282c54d6a18fcab90b2bdc6ccd49c45
fulleni <fulleni@hotmail.com>
2025-03-27 10:00:31 +0100

    feat: initial commit for shared preferences plugin
    - Added plugin files
    - Implemented shared preferences storage

--- lib/ht_kv_storage_shared_preferences.dart
+++ lib/ht_kv_storage_shared_preferences.dart
@@ -1,4 +1,5 @@
-/// A Very Good Project created by Very Good CLI.
+/// A Flutter implementation of the `HtKVStorageService` interface using
+/// the shared_preferences package.
 library;
 
 export 'src/ht_kv_storage_shared_preferences.dart';
